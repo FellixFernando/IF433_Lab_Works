1acca5d3a0a08570ae04d3b99865064e070668f5
FellixFernando <fellixwl@gmail.com>
2026-02-13 11:36:29 +0700

    week02: (task 1) implement library input system

--- src/oop_00000108757_FellixFernando/week02/Loan.kt
+++ src/oop_00000108757_FellixFernando/week02/Loan.kt
@@ -8,24 +8,45 @@ class Loan(
     var loanDuration: Int = 1
 ) {
     init {
-        if (loanDuration > 3) {
+        if (loanDuration < 0) {
+            println("WARNING: Durasi peminjaman buku tidak boleh negatif, mengatur ke 1 hari.")
+            loanDuration = 1
         }
     }
 }
 
 fun main() {
+
     val scanner = Scanner(System.`in`)
     println("--- LIBRARY FINE SYSTEM ---")
 
     print("Masukkan Judul Buku: ")
-    scanner.nextLine()
+    val bookTitle = scanner.nextLine()
 
     print("Masukkan Nama Peminjam: ")
-    scanner.nextLine()
+    val borrower = scanner.nextLine()
 
     print("Masukkan Durasi Peminjaman (dalam hari): ")
-    scanner.nextInt()
+    var loanDuration = scanner.nextInt()
 
     scanner.nextLine()
 
+    var denda = 0
+    if (loanDuration < 0) {
+        println("WARNING: Durasi peminjaman buku tidak boleh negatif, mengatur ke 1 hari.")
+        loanDuration = 1
+    }
+    if (loanDuration > 3) {
+        denda = calculateFine(loanDuration)
+    }
+    val loan = Loan(bookTitle, borrower, loanDuration)
+    println("--- Detail Peminjaman Buku ---")
+
+    println("Judul Buku: ${loan.bookTitle}")
+    println("Nama Peminjam: ${loan.borrower}")
+    println("Durasi Peminjaman: ${loan.loanDuration} hari")
+    println("Denda: Rp$denda")
+
 }
+
+fun calculateFine(loanDuration: Int) = (loanDuration - 3) * 2000
