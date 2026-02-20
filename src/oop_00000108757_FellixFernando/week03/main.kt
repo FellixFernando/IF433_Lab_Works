b4e1037102a7ebbdd9934f95a3ead08b12cc3bf9
FellixFernando <fellixwl@gmail.com>
2026-02-20 11:12:28 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000108757_FellixFernando/week03/main.kt
+++ src/oop_00000108757_FellixFernando/week03/main.kt
@@ -2,7 +2,7 @@ package oop_00000108757_FellixFernando.week03
 
 fun main() {
     val e = Employee("Fellix")
-    e.salary = -1000
+    e.salary = -1000 //error
     e.salary = 5000
     println("Gajii: ${e.salary}")
 
