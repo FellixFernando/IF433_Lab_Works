ecdb2c2ea12f19a9f6c7a31761427cf0dfc25feb
FellixFernando <fellixwl@gmail.com>
2026-02-20 10:54:31 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000108757_FellixFernando/week03/Employee.kt
+++ src/oop_00000108757_FellixFernando/week03/Employee.kt
@@ -2,5 +2,10 @@ package oop_00000108757_FellixFernando.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value
+            field = value
+        }
 }
 
---
+++ src/oop_00000108757_FellixFernando/week03/main.kt
@@ -0,0 +1,7 @@
+package oop_00000108757_FellixFernando.week03
+
+fun main() {
+    val e = Employee("Fellix")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
