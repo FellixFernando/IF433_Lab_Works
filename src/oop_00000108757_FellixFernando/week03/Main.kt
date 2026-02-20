cd972dbfaf3b63eaa7c1fcaee67c27f1ac64a5f8
FellixFernando <fellixwl@gmail.com>
2026-02-20 11:38:56 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000108757_FellixFernando/week03/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000108757_FellixFernando.week03
+
+fun main() {
+    val Weapon1 = Weapon("Excalibur")
+    Weapon1.damage = -50
+    Weapon1.damage = 9999
+    println("Tier ${Weapon1.name} : ${Weapon1.tier}")
+}
+
