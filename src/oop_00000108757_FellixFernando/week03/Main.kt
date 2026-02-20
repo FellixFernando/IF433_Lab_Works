b99307baaae763fb638a9864c2d0c27e425aed10
FellixFernando <fellixwl@gmail.com>
2026-02-20 11:46:42 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000108757_FellixFernando/week03/Main.kt
+++ src/oop_00000108757_FellixFernando/week03/Main.kt
@@ -5,5 +5,14 @@ fun main() {
     Weapon1.damage = -50
     Weapon1.damage = 9999
     println("Tier ${Weapon1.name} : ${Weapon1.tier}")
+
+    println()
+
+    val Player1 = Player("Fellix")
+    //error println("${Player1.xp}")
+    Player1.addXp(50)
+    println("Level ${Player1.name} = ${Player1.level}")
+    Player1.addXp(60)
+    Player1.addXp(180)
 }
 
