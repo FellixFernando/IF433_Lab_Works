fe7f634eeb88c74faf44296480812e4e2e5fdaf3
FellixFernando <fellixwl@gmail.com>
2026-02-27 22:59:39 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000108757_FellixFernando/week04/main.kt
+++ src/oop_00000108757_FellixFernando/week04/main.kt
@@ -3,6 +3,7 @@ package oop_00000108757_FellixFernando.week04
 fun main() {
     println("NIM : 00000108757")
     println("Nama : Fellix Fernando")
+    println("--- Task 1 ---")
     println("--- Testing Vehicle ---\n")
 
     val generalVehicle = Vehicle("Generic Vehicle")
@@ -20,4 +21,15 @@ fun main() {
     myElectricCar.accelerate()
     myElectricCar.honk()
     myElectricCar.openTrunk()
+
+    println("\n--- Task 2 ---")
+    println("--- Testing Manager ---\n")
+    val manager = Manager("Alice", 10000000)
+    manager.work()
+    println("Bonus Manager: ${manager.calculateBonus()}")
+
+    println("\n--- Testing Developer ---")
+    val developer = Developer("Bob", 8000000, "Kotlin")
+    developer.work()
+    println("Bonus Developer: ${developer.calculateBonus()}")
 }
