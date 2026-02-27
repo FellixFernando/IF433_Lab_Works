e12935bbee8ff4712bc5d0ac8dfb5379da2cbe66
FellixFernando <fellixwl@gmail.com>
2026-02-27 22:46:23 +0700

    week04: override honk method in Car

--- src/oop_00000108757_FellixFernando/week04/main.kt
+++ src/oop_00000108757_FellixFernando/week04/main.kt
@@ -1,2 +1,17 @@
 package oop_00000108757_FellixFernando.week04
 
+fun main() {
+    println("NIM : 00000108757")
+    println("Nama : Fellix Fernando")
+    println("Vehicle\n")
+
+    val myVehicle = Vehicle("Generic Vehicle")
+    myVehicle.accelerate()
+    myVehicle.honk()
+
+    println("\nCar")
+    val myCar = Car("Toyota", 4)
+    myCar.accelerate()
+    myCar.honk()
+    myCar.openTrunk()
+}
