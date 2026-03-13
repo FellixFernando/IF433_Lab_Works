2d9b6bc04e8db55026a84483616238f125d777a4
FellixFernando <fellixwl@gmail.com>
2026-03-13 10:54:16 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_00000108757_FellixFernando/week06/Clickable.kt
+++ src/oop_00000108757_FellixFernando/week06/Clickable.kt
@@ -4,9 +4,3 @@ interface Clickable {
     val name: String
     fun click()
 }
-
-class Button(override val name: String) : Clickable {
-    override fun click() {
-        println("Tombol '$name' berhasil diklik!")
-    }
-}
