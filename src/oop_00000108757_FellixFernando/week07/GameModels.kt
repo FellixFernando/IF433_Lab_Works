df847ba310c00d280095c08b425497bd11203d6f
FellixFernando <fellixwl@gmail.com>
2026-03-27 11:44:17 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000108757_FellixFernando/week07/GameModels.kt
@@ -0,0 +1,10 @@
+package oop_00000108757_FellixFernando.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(45),
+    RARE(25),
+    EPIC(10),
+    LEGENDARY(1)
+
+}
