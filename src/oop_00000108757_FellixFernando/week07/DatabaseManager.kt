f6b9d748033045f3f8841a4629319a1a85a7ae1f
FellixFernando <fellixwl@gmail.com>
2026-03-27 11:10:56 +0700

    week07: implement companion object as factory in NetworkClient

--- src/oop_00000108757_FellixFernando/week07/DatabaseManager.kt
+++ src/oop_00000108757_FellixFernando/week07/DatabaseManager.kt
@@ -1,10 +1,10 @@
 package oop_00000108757_FellixFernando.week07
 
 object DatabaseManager {
-    var connectionStatus: String = 'Disconnected'
+    var connectionStatus: String = "Disconnected"
 
     fun connect() {
-        connectionStatus = 'Connected to Server'
+        connectionStatus = "Connected to Server"
         println("Database is ready")
     }
 }
--- src/oop_00000108757_FellixFernando/week07/NetworkClient.kt
+++ src/oop_00000108757_FellixFernando/week07/NetworkClient.kt
@@ -1,7 +1,17 @@
 package oop_00000108757_FellixFernando.week07
 
 class NetworkClient private constructor(val url: String) {
+    companion object {
+        const val BASE_URL = "https://api.umn.ac.id"
+
+        fun createClient(): NetworkClient {
+            println("Membangun Network Client dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
+
     fun connect() {
         println("Connecting to $url")
     }
+
 }
