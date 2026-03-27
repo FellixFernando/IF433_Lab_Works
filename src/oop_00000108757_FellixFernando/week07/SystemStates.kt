ce463715b01ef57051f1e52e872592baacd0d7ea
FellixFernando <fellixwl@gmail.com>
2026-03-27 11:32:33 +0700

    create Apiresponse sealed class

--- src/oop_00000108757_FellixFernando/week07/SystemStates.kt
+++ src/oop_00000108757_FellixFernando/week07/SystemStates.kt
@@ -3,3 +3,9 @@ package oop_00000108757_FellixFernando.week07
 enum class SystemStates {
     STARTING, RUNNING, STOPPED
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
