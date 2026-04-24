ab9f1403019ea9e0432bd6db1ebee62279c0bba7
FellixFernando <fellixwl@gmail.com>
2026-04-24 11:04:55 +0700

    week09: (task) chain sortedBy and map for worst performers

--- src/oop_00000108757_FellixFernando/week09/TradingDashboard.kt
+++ src/oop_00000108757_FellixFernando/week09/TradingDashboard.kt
@@ -5,7 +5,7 @@ fun main() {
         TradeLog("BTCUSDT", "Long", 10, 15.5, "CLOSED"),
         TradeLog("ETHUSDT", "Short", 5, -8.2, "OPEN"),
         TradeLog("XRPUSDT", "Long", 20, 25.0, "CLOSED"),
-        TradeLog("ADAUSDT", "Short", 15, 12.3, "CLOSED"),
+        TradeLog("ADAUSDT", "Short", 15, -12.3, "CLOSED"),
         TradeLog("ETHUSDT", "Long", 30, 7.6, "OPEN"),
         TradeLog("BTCUSDT", "Short", 25, 10.0, "OPEN"),
         TradeLog("XRPUSDT", "Long", 5, -5.0, "CLOSED")
@@ -17,5 +17,8 @@ fun main() {
 
     val topPerformerString = winningTrades.sortedByDescending { it.roe }
         .map { println("WIN [${it.pair} - ${it.position}]: +${it.roe}% (Leverage: ${it.leverage}x)") }
+
+    val worstPerformerString = losingTrades.sortedBy { it.roe }
+        .map { println("LOSS [${it.pair} - ${it.position}]: ${it.roe}% (Leverage: ${it.leverage}x)") }
 
 }
