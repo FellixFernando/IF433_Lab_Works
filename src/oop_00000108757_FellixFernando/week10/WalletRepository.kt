39935f2981b4a95b8e35f6c9531148ad1a1933ec
FellixFernando <fellixwl@gmail.com>
2026-05-08 10:53:44 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000108757_FellixFernando/week10/WalletRepository.kt
+++ src/oop_00000108757_FellixFernando/week10/WalletRepository.kt
@@ -1,6 +1,10 @@
 package oop_00000108757_FellixFernando.week10
 
-class WalletRepository<T> {
+interface HasName {
+    val name: String
+}
+
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
 
     fun add(item: T) {
@@ -9,5 +13,11 @@ class WalletRepository<T> {
 
     fun getAll(): List<T> {
         return items
+    }
+
+    fun getByName(name: String): T? {
+        return items.firstOrNull { item ->
+            (item as? HasName)?.name == name
+        }
     }
 }
