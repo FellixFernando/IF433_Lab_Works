8091d99cbb2d28d9a0c2ad033d0e93ba6320055d
FellixFernando <fellixwl@gmail.com>
2026-05-08 10:56:04 +0700

    week10: (task) populate coin repository

--- src/oop_00000108757_FellixFernando/week10/CryptoDashboard.kt
+++ src/oop_00000108757_FellixFernando/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000108757_FellixFernando.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("Bitcoin", 1.5))
+    coinRepo.add(Coin("Ethereum", 10.0))
+    coinRepo.add(Coin("USDT", 15.0))
 }
