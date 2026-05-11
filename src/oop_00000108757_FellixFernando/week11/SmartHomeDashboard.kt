2b2da8bd1b5d03f218381902ffc493c310e55a88
FellixFernando <fellixwl@gmail.com>
2026-05-11 17:52:51 +0700

    week11: (task) implement device search with let

--- src/oop_00000108757_FellixFernando/week11/SmartHomeDashboard.kt
+++ src/oop_00000108757_FellixFernando/week11/SmartHomeDashboard.kt
@@ -10,7 +10,7 @@ fun main() {
     }.also {
         homeDevices.add(it)
     }
-    val kamera1 = SmartDevice("Ezviz Outdoor", "").apply {
+    val kamera1 = SmartDevice("Ezviz Outdoor", "Camera").apply {
         isOnline = true
         powerLoad = 5
     }.also {
@@ -26,5 +26,11 @@ fun main() {
 
     val petFeeder1 = SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10).also {
         homeDevices.add(it)
+    }
+
+    val searchResult = homeDevices.find {
+        it.category == "Camera"
+    }?.let {
+        println(it.diagnose())
     }
 }
